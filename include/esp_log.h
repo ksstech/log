3c134efe654ef91789ef6c64752e49bf0f812162
André Maree <ammaree@users.noreply.github.com>
2022-06-13 14:41:56 +0200

    Change to use CRLF on all messages

--- include/esp_log.h
+++ include/esp_log.h
@@ -291,7 +291,7 @@ void vSyslog(int Priority, const char * MsgID, const char * format, ...);
 #define LOG_RESET_COLOR
 #endif //CONFIG_LOG_COLORS
 
-#define LOG_FORMAT(letter, format)  LOG_COLOR_ ## letter #letter " (%u) %s: " format LOG_RESET_COLOR "\n"
+#define LOG_FORMAT(letter, format)  LOG_COLOR_ ## letter #letter " (%u) %s: " format LOG_RESET_COLOR "\r\n"
 //#define LOG_SYSTEM_TIME_FORMAT(letter, format)  LOG_COLOR_ ## letter #letter " (%s) %s: " format LOG_RESET_COLOR "\n"
 
 /** @endcond */
